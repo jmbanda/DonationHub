99df52a8cd9ebb1731cbcd8e5f5344c67799feec
Juan M. Banda <jmbanda@stanford.edu>
2015-08-23 12:36:18 -0700

    latest changes
    changes

--- donate.php
+++ donate.php
@@ -12,9 +12,52 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <script src="js/ie-emulation-modes-warning.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.2.3/jquery.payment.min.js"></script>
+   <!-- Vendor libraries -->
+   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+   <style type="text/css">	
+		/* CSS for Credit Card Payment form */
+		.credit-card-box .panel-title {
+			display: inline;
+			font-weight: bold;
+		}
+		.credit-card-box .form-control.error {
+			border-color: red;
+			outline: 0;
+			box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(255,0,0,0.6);
+		}
+		.credit-card-box label.error {
+		  font-weight: bold;
+		  color: red;
+		  padding: 2px 8px;
+		  margin-top: 2px;
+		}
+		.credit-card-box .payment-errors {
+		  font-weight: bold;
+		  color: red;
+		  padding: 2px 8px;
+		  margin-top: 2px;
+		}
+		.credit-card-box label {
+			display: block;
+		}
+		/* The old "center div vertically" hack */
+		.credit-card-box .display-table {
+			display: table;
+		}
+		.credit-card-box .display-tr {
+			display: table-row;
+		}
+		.credit-card-box .display-td {
+			display: table-cell;
+			vertical-align: middle;
+			width: 50%;
+		}
+		/* Just looks nicer */
+		.credit-card-box .panel-heading img {
+			min-width: 180px;
+		}
+	</style>
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -151,80 +194,158 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.min.js"></script>
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/ie10-viewport-bug-workaround.js"></script>    
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.2.3/jquery.payment.min.js"></script>
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
     
 <script type="text/javascript">
-$(function () {
-	/* Fancy restrictive input formatting via jQuery.payment library*/
-	$('input[name=cardNumber]').payment('formatCardNumber');
-	$('input[name=cardCVC]').payment('formatCardCVC');
-	$('input[name=cardExpiry').payment('formatCardExpiry');
-	
-	/* Form validation using Stripe client-side validation helpers */
-	jQuery.validator.addMethod("cardNumber", function(value, element) {
-		return this.optional(element) || Stripe.card.validateCardNumber(value);
-	}, "Please specify a valid credit card number.");
-	
-	jQuery.validator.addMethod("cardExpiry", function(value, element) {    
-		/* Parsing month/year uses jQuery.payment library */
-		value = $.payment.cardExpiryVal(value);
-		return this.optional(element) || Stripe.card.validateExpiry(value.month, value.year);
-	}, "Invalid expiration date.");
-	
-	jQuery.validator.addMethod("cardCVC", function(value, element) {
-		return this.optional(element) || Stripe.card.validateCVC(value);
-	}, "Invalid CVC.");
-	
-	validator = $form.validate({
-		rules: {
-			cardNumber: {
-				required: true,
-				cardNumber: true            
-			},
-			cardExpiry: {
-				required: true,
-				cardExpiry: true
-			},
-			cardCVC: {
-				required: true,
-				cardCVC: true
-			}
-		},
-		highlight: function(element) {
-			$(element).closest('.form-control').removeClass('success').addClass('error');
-		},
-		unhighlight: function(element) {
-			$(element).closest('.form-control').removeClass('error').addClass('success');
-		},
-		errorPlacement: function(error, element) {
-			$(element).closest('.form-group').append(error);
-		}
-	});
-	
-	paymentFormReady = function() {
-		if ($form.find('[name=cardNumber]').hasClass("success") &&
-			$form.find('[name=cardExpiry]').hasClass("success") &&
-			$form.find('[name=cardCVC]').val().length > 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	$form.find('[type=submit]').prop('disabled', true);
-	var readyInterval = setInterval(function() {
-		if (paymentFormReady()) {
-			$form.find('[type=submit]').prop('disabled', false);
-			clearInterval(readyInterval);
-		}
-	}, 250);
+	/*
+The MIT License (MIT)
 
-	
+Copyright (c) 2015 William Hilton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+var $form = $('#payment-form');
+$form.on('submit', payWithStripe);
+
+/* If you're using Stripe for payments */
+function payWithStripe(e) {
+    e.preventDefault();
+
+    /* Visual feedback */
+    $form.find('[type=submit]').html('Validating <i class="fa fa-spinner fa-pulse"></i>');
+
+    var PublishableKey = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'; // Replace with your API publishable key
+    Stripe.setPublishableKey(PublishableKey);
+    
+    /* Create token */
+    var expiry = $form.find('[name=cardExpiry]').payment('cardExpiryVal');
+    var ccData = {
+        number: $form.find('[name=cardNumber]').val().replace(/\s/g,''),
+        cvc: $form.find('[name=cardCVC]').val(),
+        exp_month: expiry.month, 
+        exp_year: expiry.year
+    };
+    
+    Stripe.card.createToken(ccData, function stripeResponseHandler(status, response) {
+        if (response.error) {
+            /* Visual feedback */
+            $form.find('[type=submit]').html('Try again');
+            /* Show Stripe errors on the form */
+            $form.find('.payment-errors').text(response.error.message);
+            $form.find('.payment-errors').closest('.row').show();
+        } else {
+            /* Visual feedback */
+            $form.find('[type=submit]').html('Processing <i class="fa fa-spinner fa-pulse"></i>');
+            /* Hide Stripe errors on the form */
+            $form.find('.payment-errors').closest('.row').hide();
+            $form.find('.payment-errors').text("");
+            // response contains id and card, which contains additional card details            
+            console.log(response.id);
+            console.log(response.card);
+            var token = response.id;
+            // AJAX - you would send 'token' to your server here.
+            $.post('/account/stripe_card_token', {
+                    token: token
+                })
+                // Assign handlers immediately after making the request,
+                .done(function(data, textStatus, jqXHR) {
+                    $form.find('[type=submit]').html('Payment successful <i class="fa fa-check"></i>').prop('disabled', true);
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    $form.find('[type=submit]').html('There was a problem').removeClass('success').addClass('error');
+                    /* Show Stripe errors on the form */
+                    $form.find('.payment-errors').text('Try refreshing the page and trying again.');
+                    $form.find('.payment-errors').closest('.row').show();
+                });
+        }
+    });
+}
+/* Fancy restrictive input formatting via jQuery.payment library*/
+$('input[name=cardNumber]').payment('formatCardNumber');
+$('input[name=cardCVC]').payment('formatCardCVC');
+$('input[name=cardExpiry').payment('formatCardExpiry');
+
+/* Form validation using Stripe client-side validation helpers */
+jQuery.validator.addMethod("cardNumber", function(value, element) {
+    return this.optional(element) || Stripe.card.validateCardNumber(value);
+}, "Please specify a valid credit card number.");
+
+jQuery.validator.addMethod("cardExpiry", function(value, element) {    
+    /* Parsing month/year uses jQuery.payment library */
+    value = $.payment.cardExpiryVal(value);
+    return this.optional(element) || Stripe.card.validateExpiry(value.month, value.year);
+}, "Invalid expiration date.");
+
+jQuery.validator.addMethod("cardCVC", function(value, element) {
+    return this.optional(element) || Stripe.card.validateCVC(value);
+}, "Invalid CVC.");
+
+validator = $form.validate({
+    rules: {
+        cardNumber: {
+            required: true,
+            cardNumber: true            
+        },
+        cardExpiry: {
+            required: true,
+            cardExpiry: true
+        },
+        cardCVC: {
+            required: true,
+            cardCVC: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-control').removeClass('success').addClass('error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-control').removeClass('error').addClass('success');
+    },
+    errorPlacement: function(error, element) {
+        $(element).closest('.form-group').append(error);
+    }
 });
-</script>    
+
+paymentFormReady = function() {
+    if ($form.find('[name=cardNumber]').hasClass("success") &&
+        $form.find('[name=cardExpiry]').hasClass("success") &&
+        $form.find('[name=cardCVC]').val().length > 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$form.find('[type=submit]').prop('disabled', true);
+var readyInterval = setInterval(function() {
+    if (paymentFormReady()) {
+        $form.find('[type=submit]').prop('disabled', false);
+        clearInterval(readyInterval);
+    }
+}, 250);
+	</script>
     
 </body>
 </html>
